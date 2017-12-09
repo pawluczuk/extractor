af6687c2911fb2b77398c92b122c4eebcb33802d
pawluczuk <pawluczuk.monika@gmail.com>
2017-12-09 18:30:17 +0000

    added cluster

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose')
   , path = require('path')
   , fs = require('fs')
   , async = require('async')
+  , cluster = require('cluster')
   , models = require('./models')
   , debug = require('debug')('extractor:index')
   , extractor = require('./extractor')({models})
   , db = process.env.MONGO_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/extractor'
   , DIRECTORY = process.argv.length >= 3 ? process.argv[2] : '../rdfs/epub'
-  , PROCESS_MAX_OPEN_FILES = process.argv.length >= 4 ? process.argv[3] : 1000
+  , PROCESS_MAX_OPEN_FILES = process.argv.length >= 4 ? process.argv[3] : 256
+  , WORKERS_NUMBER = require('os').cpus().length
   ;
 
 mongoose.Promise = global.Promise;
@@ -24,13 +26,69 @@ fs.readdir(DIRECTORY, (err, dirs) => {
     process.exit(0);
   }
 
-  const fileList = (dirs || [])
-    .filter(d => !d.startsWith('.'))
-    .map(d => path.join(DIRECTORY, d, `pg${d}.rdf`));
+  if (cluster.isMaster) {
+    let currentlyWorking = 0;
+    let fileList = (dirs || [])
+      .filter(d => !d.startsWith('.'))
+      .map(d => path.join(DIRECTORY, d, `pg${d}.rdf`));
 
-  async.everyLimit(fileList, PROCESS_MAX_OPEN_FILES, extractor.processMetadata, (err) => {
-    console.log(err)
-    //console.log(results)
-    mongoose.disconnect();
-  });
+    fileList = fileList.slice(0, 1000);
+
+    for (let i = 0; i < WORKERS_NUMBER; i++) {
+      cluster.fork();
+    }
+
+    cluster.on('online', function(worker) {
+      debug('Worker ' + worker.process.pid + ' is online');
+      currentlyWorking += 1;
+    });
+
+    cluster.on('exit', function(worker, code, signal) {
+      debug('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+      if (fileList.length) {
+        debug('Starting a new worker');
+        cluster.fork();
+      } else {
+        currentlyWorking -= 1;
+        debug('Still working ', currentlyWorking)
+        if (!currentlyWorking) {
+          debug('All workers finished working');
+          mongoose.disconnect(() => {
+            process.exit();
+          });
+        }
+      }
+    });
+
+    cluster.on('message', function(worker, code, signal) {
+      debug('Master received message', code);
+      if (['finish', 'ready'].indexOf(code) > -1) {
+        debug('Master sending new file. Left files:', fileList.length);
+        if (fileList.length) {
+          worker.send({
+            type: 'processfile',
+            from: 'master',
+            file: fileList.pop()
+          });
+        } else {
+          worker.send({
+            type: 'done'
+          });
+        }
+      }
+    });
+  } else {
+    process.send('ready');
+
+    process.on('message', function(task) {
+      if (task.type === 'processfile') {
+        extractor.processMetadata(task.file, (err) => {
+          debug('processMetadata err', err);
+          process.send('finish');
+        });
+      } else if (task.type === 'done') {
+        process.exit();
+      }
+    });
+  }
 });
